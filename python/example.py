9fd337cee3f9701c4a062092b7738e4229637a06
Reid Williams <rwilliams@ideo.com>
2018-06-14 10:56:07 -0700

    Update example.py

--- python/example.py
+++ python/example.py
@@ -1,5 +1,10 @@
 import time
 
+# PyFi lets you send messages to a client two ways:
+# by calling pyfi_message or by returning a value.
+# pyfi_message is automagically injected into the environment
+# by PyFi
+
 def slow_task(time_per_tick):
     total_time = 0
     for i in range(0,100):
@@ -9,3 +14,4 @@ def slow_task(time_per_tick):
         total_time += time_per_tick
     # return value to client   
     return(total_time)
+
