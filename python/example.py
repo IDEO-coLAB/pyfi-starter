a1dd2ed8aefaeb99d30b3b9903d4fffc3387248c
Reid Williams <rwilliams@ideo.com>
2018-06-14 10:53:25 -0700

    Update example.py

--- python/example.py
+++ python/example.py
@@ -3,7 +3,9 @@ import time
 def slow_task(time_per_tick):
     total_time = 0
     for i in range(0,100):
+        # send message to client's onMessage handler
         pyfi_message({"progress": i})
         time.sleep(time_per_tick)
         total_time += time_per_tick
+    # return value to client   
     return(total_time)
